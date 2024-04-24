d118f1a9bd3c001aa130e9b182b3a0d7baf89498
Adrián Sanchez Romero <adriansrom@ext.inditex.com>
2024-04-24 18:08:23 +0200

    store mode

--- src/Layout.js
+++ src/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Toaster } from 'sonner'
 import FooterTabs from './components/footerTabs/FooterTabs'
 import './Layout.css'
 
 export default function Layout() {
   return (
     <div className='layoutContainer'>
+      <Toaster />
       <Outlet />
       <FooterTabs />
     </div>
